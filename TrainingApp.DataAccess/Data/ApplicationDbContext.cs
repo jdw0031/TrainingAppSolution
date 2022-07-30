73124a8dcd0e0a57777233200cec6d18c668c053
Software Jedi <juliandwilliams98@gmail.com>
2022-07-30 11:02:26 -0400

    Fixing object refernece issue

--- TrainingApp.DataAccess/Data/ApplicationDbContext.cs
+++ TrainingApp.DataAccess/Data/ApplicationDbContext.cs
@@ -7,7 +7,16 @@ namespace TrainingApp.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
 
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 
@@ -20,10 +29,6 @@ namespace TrainingApp.Data
 
         public DbSet<TrainerSchedule> TrainerSchedules { get; set; }
 
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-
-        }
+        
     }
 }
--- TrainingApp/Program.cs
+++ TrainingApp/Program.cs
@@ -9,37 +9,76 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using TrainingApp;
 using Microsoft.AspNetCore;
 
-namespace TrainingApp
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDefaultIdentity<ApplicationUser>()
+            .AddEntityFrameworkStores<ApplicationDbContext>();
+//builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddTransient<ITrainerRepo, TrainerRepo>();
+builder.Services.AddTransient<ITrainerScheduleRepo, TrainerScheduleRepo>();
+builder.Services.AddTransient<IApplicationUserRepo, ApplicationUserRepo>();
+builder.Services.AddTransient<ITrainingSessionRepo, TrainingSessionRepo>();
+builder.Services.AddTransient<IEmailSender, EmailSender>();
+//builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        // the sole purpose of this code is to help the connect to the database
-        public static void Main(string[] args)
-        {
-            //CreateHostBuilder(args).Build().Run();
-            var host = CreateWebHostBuilder(args).Build();
-
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-
-                try
-                {
-                    DbInitializer.Initialize(services).Wait();
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occured creating the DB.");
-                }
-            }
-
-            host.Run();
-
-        } // end of main method
-
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                    .UseStartup<Startup>();
-    }
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
+//namespace TrainingApp
+//{
+//    public class Program
+//    {
+//        // the sole purpose of this code is to help the connect to the database
+//        public static void Main(string[] args)
+//        {
+//            //CreateHostBuilder(args).Build().Run();
+//            var host = CreateWebHostBuilder(args).Build();
+
+//            using (var scope = host.Services.CreateScope())
+//            {
+//                var services = scope.ServiceProvider;
+
+//                try
+//                {
+//                    DbInitializer.Initialize(services).Wait();
+//                }
+//                catch (Exception ex)
+//                {
+//                    var logger = services.GetRequiredService<ILogger<Program>>();
+//                    logger.LogError(ex, "An error occured creating the DB.");
+//                }
+//            }
+
+//            host.Run();
+
+//        } // end of main method
+
+//        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+//            WebHost.CreateDefaultBuilder(args)
+//                    .UseStartup<Startup>();
+//            }
+//}
